7fcd3109e023025a553fd4ded7ea997248473132
Halina Tulchii <halendeihalyna@gmail.com>
2024-06-14 20:37:39 +0300

    added google map and slowling appearance for it

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,27 +1,43 @@
 import React, { useState } from 'react'
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
-import map from '../../images/map.png'
+// import map from '../../images/map.png'
 import '../../sass/components/_location.scss'
-import { motion } from 'framer-motion'; // Імпортуємо framer-motion
+import { AnimatePresence, motion } from 'framer-motion'; // Імпортуємо framer-motion
 import { transition1 } from '../../transitions/transitions';
 
 const Location = () => {
     const [show, setShow] = useState(false)
     const onShowClick = () => {
-        if (show === false) {
-            setShow(true)
-        } else setShow(false)
+        setShow(prev =>!prev)
     }
   return (
       <div className='location'>
           <div>
-            <button onClick={onShowClick} className='location__btn title'>Місцезнаходження {show === true ? <SlArrowUp className='location__arrow location__arrow--down'/> : <SlArrowDown className='location__arrow location__arrow--down'/>}</button>
-          {show && <motion.div
-              initial={{ scale: 0}}
-              animate={{ scale: 1}}
-              exit={{ scale: 0}}
+        <button onClick={onShowClick} className='location__btn title'>
+          Місцезнаходження {show ? <SlArrowUp className='location__arrow location__arrow--down' /> : <SlArrowDown className='location__arrow location__arrow--down' />}
+        </button>
+      <AnimatePresence>      
+      {show && (
+        <motion.div
+        initial={{ height: 0, opacity: 0 }}
+        animate={{ height: 'auto', opacity: 1 }}
+        exit={{ height: 0, opacity: 0 }}
               transition={transition1}
-              className='location__img--box'><img src={map} alt='map' className='location__img' /></motion.div>}
+              style={{ overflow: 'hidden' }}
+          className='location__img--box'
+        >
+          <iframe
+            title='Google Map'
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2643.5219867974047!2d35.073501199999995!3d48.5040575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d959826027371b%3A0x4008234a3659c56b!2z0KTQvtGC0L7RgdGC0YPQtNGW0Y8gRm90b3ggU3R1ZGlv!5e0!3m2!1sru!2sua!4v1718385080169!5m2!1sru!2sua"
+            width="290"
+            height="250"
+            style={{ border: 0 }}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </motion.div>
+      )}
+    </AnimatePresence>
           </div>
           
     </div>
