1bbd3b8da4a68f4385786316752c251a7d33c709
Halina Tulchii <halendeihalyna@gmail.com>
2024-06-30 18:55:57 +0300

    added booking  and payment posibilities an

--- src/components/Booking/PaymentContent.tsx
+++ src/components/Booking/PaymentContent.tsx
@@ -1,8 +1,163 @@
+// import React, { useState } from "react";
+// import { motion } from "framer-motion";
+// import { IoClose } from "react-icons/io5";
+// import "../../sass/components/_payment.scss";
+// import axios from "axios";
+
+// interface PaymentContentProps {
+//   closeModal: () => void;
+//   selectedData: Date | null;
+//   selectedSlot: { start: string; end: string } | null;
+//   selectedPrice: number;
+// }
+
+// const PaymentContent: React.FC<PaymentContentProps> = ({
+//   selectedData,
+//   selectedSlot,
+//   selectedPrice,
+//   closeModal,
+// }) => {
+//   const [form, setForm] = useState({
+//     name: "",
+//     number: "",
+//     email: "",
+//   });
+
+//   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { value, name } = e.target;
+
+//     setForm((prevState) => ({
+//       ...prevState,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleSubmit = async (e: React.FormEvent) => {
+//     e.preventDefault();
+//     try {
+//       console.log("Submitting form...");
+//       const response = await axios.post(
+//         `https://art-studio-api.onrender.com/api/bookings/book?price=1`,
+//         // {
+//         //   name: form.name,
+//         //   number: form.number,
+//         //   email: form.email,
+//         // }
+//       );
+//       console.log(response)
+
+//       const booking = response.data.booking;
+//       const bookingId = booking._id;
+
+//       console.log("Booking request status:", response.status);
+//       console.log("Booking request response:", booking);
+
+//       setForm({
+//         name: "",
+//         number: "",
+//         email: "",
+//       });
+
+//       setTimeout(async () => {
+//         console.log("Fetching payment URL...");
+//         const paymentResponse = await axios.get(
+//           `https://art-studio-api.onrender.com/api/payments/payment-form?currency=UAH&productName[]=photosession&productCount[]=1&bookingId=${bookingId}`
+//         );
+
+//         console.log("Payment request status:", paymentResponse.status);
+//         console.log("Payment request response:", paymentResponse.data);
+
+//         if (paymentResponse.status === 200) {
+//           window.location.href = `https://art-studio-api.onrender.com/api/payments/payment-form?currency=UAH&productName[]=photosession&productCount[]=1&bookingId=${bookingId}`;
+//         } else {
+//           console.error("Error in payment request:", paymentResponse.data);
+//         }
+//       }, 10000);
+//     } catch (error) {
+//       console.error("Error submitting the form", error);
+//     }
+//   };
+
+
+
+
+//   return (
+//     <motion.div
+//       onClick={(e) => e.stopPropagation()}
+//       initial={{ y: "100%" }}
+//       animate={{ y: "0%" }}
+//       exit={{ y: "100%" }}
+//       transition={{ duration: 0.5 }}
+//       className="feedback__content container"
+//     >
+//       <div className="box__payment">
+//         <h1 className="title">Оплата</h1>
+//         <button className="payment__btn" onClick={closeModal}>
+//           <IoClose className="feedback__close" />
+//         </button>
+//       </div>
+
+//       <div className="box__booking">
+//         <h2 className="payment__subtitle">Бронювання:</h2>
+//         <p className="payment__text">
+//           {selectedData?.toLocaleDateString()}{" "}
+//           {selectedSlot ? `${selectedSlot.start}-${selectedSlot.end}` : ""}
+//         </p>
+//       </div>
+
+//       <div className="box__line" />
+
+//       <div className="feedback__formbox">
+//         <form action="" className="form" onSubmit={handleSubmit}>
+//           <div className="form__inputs">
+//             <input
+//               name="name"
+//               value={form.name}
+//               onChange={onChange}
+//               placeholder="Ваше ім’я"
+//               className="form__input form__name"
+//             />
+//             <input
+//               name="number"
+//               value={form.number}
+//               onChange={onChange}
+//               placeholder="Номер телефону"
+//               className="form__input form__name"
+//             />
+//             <input
+//               name="email"
+//               value={form.email}
+//               onChange={onChange}
+//               placeholder="E-mail адреса"
+//               className="form__input form__name"
+//             />
+//           </div>
+
+//           <div className="box__topay">
+//             <h2 className="payment__subtitle">До сплати:</h2>
+//             <p className="payment__text">{selectedPrice} грн</p>
+//           </div>
+
+          
+
+//           <button type="submit" className="btn btn__booking">
+//             Сплатити
+//           </button>
+//         </form>
+//       </div>
+//     </motion.div>
+//   );
+// };
+
+// export default PaymentContent;
+
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { IoClose } from "react-icons/io5";
 import "../../sass/components/_payment.scss";
 import axios from "axios";
+import { toDate } from "date-fns-tz";
+
 
 interface PaymentContentProps {
   closeModal: () => void;
@@ -10,6 +165,11 @@ interface PaymentContentProps {
   selectedSlot: { start: string; end: string } | null;
   selectedPrice: number;
 }
+
+// interface TimeSlot {
+//   start: string;
+//   end: string;
+// }
 
 const PaymentContent: React.FC<PaymentContentProps> = ({
   selectedData,
@@ -34,16 +194,35 @@ const PaymentContent: React.FC<PaymentContentProps> = ({
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (!selectedData || !selectedSlot) {
+      console.error("Missing booking date or slot");
+      return;
+    }
+
+    const timeZone = "Europe/Kiev"; // Ваша часова зона
+    const localDate = new Date(
+      `${selectedData.toISOString().split("T")[0]}T${selectedSlot.start}:00`
+    );
+    const zonedDate = toDate(localDate, { timeZone });
+  
+    // Переводимо дату в ISO формат для відправки на сервер
+    const bookingDate = zonedDate.toISOString(); 
+
     try {
       console.log("Submitting form...");
+      console.log("Form data:", form);
+      console.log("Selected data:", selectedData);
+      console.log("Selected slot:", selectedSlot);
       const response = await axios.post(
         `https://art-studio-api.onrender.com/api/bookings/book?price=1`,
-        // {
-        //   name: form.name,
-        //   number: form.number,
-        //   email: form.email,
-        // }
+        {
+          name: form.name,
+          number: form.number,
+          email: form.email,
+          bookingDate
+        }
       );
+      
 
       const booking = response.data.booking;
       const bookingId = booking._id;
@@ -71,14 +250,11 @@ const PaymentContent: React.FC<PaymentContentProps> = ({
         } else {
           console.error("Error in payment request:", paymentResponse.data);
         }
-      }, 4000);
+      }, 1000);
     } catch (error) {
       console.error("Error submitting the form", error);
     }
   };
-
-
-
 
   return (
     <motion.div
@@ -136,8 +312,6 @@ const PaymentContent: React.FC<PaymentContentProps> = ({
             <h2 className="payment__subtitle">До сплати:</h2>
             <p className="payment__text">{selectedPrice} грн</p>
           </div>
-
-          
 
           <button type="submit" className="btn btn__booking">
             Сплатити
