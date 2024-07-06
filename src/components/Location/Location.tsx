c7cddc10214ee04ceb317325a07e2eaf818e3169
Halina Tulchii <halendeihalyna@gmail.com>
2024-07-06 21:11:43 +0300

    changed styles

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -33,9 +33,9 @@ const Location = () => {
               <iframe
                 title="Google Map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2643.5219867974047!2d35.073501199999995!3d48.5040575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d959826027371b%3A0x4008234a3659c56b!2z0KTQvtGC0L7RgdGC0YPQtNGW0Y8gRm90b3ggU3R1ZGlv!5e0!3m2!1sru!2sua!4v1718385080169!5m2!1sru!2sua"
-                width="320"
+                width="290"
                 height="250"
-                style={{ border: "solid 1px #C2B5A4", borderRadius: "8px" }}
+                style={{ border: "solid 1px #C2B5A4", borderRadius: "8px", marginBottom: "5px" }}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
